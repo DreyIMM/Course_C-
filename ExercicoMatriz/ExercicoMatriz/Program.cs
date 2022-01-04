2d49594783aec3bc185d746139084ae5f277d3bf
DreyIMM <andreydario26@gmail.com>
2022-01-03 22:18:11 -0300

    Includ of outher reasource

---
+++ ExercicoMatriz/ExercicoMatriz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicoMatriz {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Digite o tamanho da matriz: ");
+            int tMatriz = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[tMatriz, tMatriz];
+           
+                for (int i = 0; i< tMatriz; i++) {
+
+                string[] values = Console.ReadLine().Split(' ');
+
+                    
+                    for(int j=0; j< tMatriz; j++) {
+                        mat[i, j] = int.Parse(values[j]);
+                    }
+
+                }                                            
+
+        }
+    }
+}
