631999d8c2e17231ea42b68e6fe76b87d8e136bf
DreyIMM <andreydario26@gmail.com>
2022-01-04 13:35:53 -0300

    If ternario

---
+++ IfTernario/IfTernario/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace IfTernario {
+    class Program {
+        static void Main(string[] args) {
+
+            double preco = double.Parse(Console.ReadLine());
+
+
+            double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
