d3cf0936ea3a8111bd13f761ecf804dd30b78bfc
DreyIMM <andreydario26@gmail.com>
2022-01-11 19:41:18 -0300

    Praticando conceito de lista com 1-N

---
+++ PostAndComment/PostAndComment/Entities/Post.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostAndComment.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Like { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post() { }
+        public Post(DateTime moment, string title, string content,int like)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Like = like;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Like);
+            sb.Append("Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Commets: ");
+            foreach(Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ PostAndComment/PostAndComment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using PostAndComment.Entities;
+
+namespace PostAndComment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awsome !");
+            Post p1 = new Post(
+                DateTime.Now,
+                "Traveling to New ",
+                "I'm going to visiti this wonderful contry !",
+                12              
+                );
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+
+            Console.WriteLine();
+
+            Console.WriteLine(p1);
+
+        }
+    }
+}
