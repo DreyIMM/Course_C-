0068ee499412788ef5fe11806e859a31772c6359
DreyIMM <andreydario26@gmail.com>
2022-01-12 17:32:51 -0300

    Esqueleto feito

--- Lanchonete/Lanchonete/Program.cs
+++ Lanchonete/Lanchonete/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using Lanchonete.Entities;
+using Lanchonete.Entities.Enums;
 
 namespace Lanchonete
 {
@@ -10,6 +10,39 @@ namespace Lanchonete
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("- -Insira dados do cliente - - ");
+            
+            Console.Write("Nome: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Data de aniversário: ");
+            DateTime birth = DateTime.Parse(Console.ReadLine());
+                       
+            Console.Write("Insira o status do produto: ");
+            OrderStatus orderStatus = (OrderStatus)Enum.Parse(typeof(OrderStatus), Console.ReadLine());
+
+            Console.Write("Quantos itens para o pedido: ");
+            int totalItens = int.Parse(Console.ReadLine());
+
+            
+
+            for(int i=1; i <= totalItens; i++)
+            {
+                Console.WriteLine($"Pedido{i} - Insira os dados");
+                Console.Write("Nome do produto: ");
+                string nameProduct = Console.ReadLine();
+                Console.Write("Preço do produto: ");
+                double precoProduto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantidade: ");
+                int qtdProduct = int.Parse(Console.ReadLine());
+
+            }
+
+
+
 
 
 
