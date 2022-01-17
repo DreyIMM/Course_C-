fd11fb4056212629aba3d0ac97c6a155738456c9
DreyIMM <andreydario26@gmail.com>
2022-01-17 12:25:57 -0300

    Downcasting and UpCasting

---
+++ Herença/Herença/Entities/SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herença.Entities
+{
+    class SavingsAccount :Account
+    {
+
+        public double IntersesaRate { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance,double intersesaRate)
+            :base(number,holder,balance)
+        {
+            IntersesaRate = intersesaRate;
+        }
+
+        public void UpdateBalacen()
+        {
+            Balance += Balance * IntersesaRate;
+        }
+
+    }
+}
