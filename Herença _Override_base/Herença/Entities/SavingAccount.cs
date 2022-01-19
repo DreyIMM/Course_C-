ed3a278572637ff826f74fe42a5a211072f04fb9
DreyIMM <andreydario26@gmail.com>
2022-01-18 22:06:47 -0300

    Incluindo base

---
+++ Herença _Override_base/Herença/Entities/SavingAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herença.Entities
+{
+    class SavingAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+
+        public SavingAccount() { }
+
+        public SavingAccount(int number, string holder, double balance, double interesRate)
+        {
+            InterestRate = interesRate;
+        }
+
+
+    }
+}
---
+++ Herença _Override_base/Herença/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Herença.Entities;
+
+namespace Herença
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1001, "Andrey", 500.0);
+            Account acc2 = new SavingsAccount(1002,"Ana",500,0.01);
+
+            acc1.Whitdram(10.0);
+            acc2.Whitdram(10.0);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+        }
+    }
+}
