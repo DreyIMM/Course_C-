3acef1e9e3354dfd7f1fd016b08ed3ea18587f0e
DreyIMM <andreydario26@gmail.com>
2022-01-21 13:17:51 -0300

    Done

--- EmployeeForHours/EmployeeForHours/Program.cs
+++ EmployeeForHours/EmployeeForHours/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using EmployeeForHours.Entitites;
+using System.Globalization;
 
 namespace EmployeeForHours
 {
@@ -10,6 +9,47 @@ namespace EmployeeForHours
     {
         static void Main(string[] args)
         {
+            List<Employee> list = new List<Employee>(); 
+            
+            Console.Write("Quantidade de funcionarios ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i=1; i <= n; i++)
+            {
+                Console.Write($"Employee  #{i} data: ");
+                Console.Write("Funcinário terceirizado (y/n)? ");
+                char c = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (c == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+                
+            }
+
+                Console.WriteLine() ;
+                Console.Write("---- Payments ------");
+
+                foreach(Employee em in list)
+                {
+                    Console.WriteLine(em.Name);
+                    Console.Write("R$" +em.Payment().ToString("F2",CultureInfo.InvariantCulture));
+                }
+
+            
+
         }
     }
 }
