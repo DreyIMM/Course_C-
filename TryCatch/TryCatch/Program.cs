a9ca5aa2b52beb2db1791f6058424d12c0ef6476
DreyIMM <andreydario26@gmail.com>
2022-01-22 20:58:10 -0300

    Try and Catch

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            try { 
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("erro " + e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Erro de formatação " +e.Message);
+            }
+        }
+    }
+}
