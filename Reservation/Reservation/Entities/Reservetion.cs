b1fc3ed30fd37c52ef571bb1cddb7542c3a0460d
DreyIMM <andreydario26@gmail.com>
2022-01-23 13:56:39 -0300

    Start project

---
+++ Reservation/Reservation/Entities/Reservetion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Reservation.Entities
+{
+    class Reservetion
+    {
+        public int RoomNumber{ get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservetion() { }
+
+        public Reservetion(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public override string ToString()
+        {
+            return "Quarto "
+                + RoomNumber
+                + ", chek-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out"
+                + CheckOut.ToString("dd/MM/yyyy")
+                + Duration()
+                + " noites";
+        }
+    }
+}
---
+++ Reservation/Reservation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reservation.Entities
+namespace Reservation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Numero do quarto ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Check-in data: (dd/MM/yyyy) ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out data: (dd/MM/yyyy) ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            Reservetion reservertion = new Reservetion(number, checkIn, checkOut);
+
+
+        }
+    }
+}
