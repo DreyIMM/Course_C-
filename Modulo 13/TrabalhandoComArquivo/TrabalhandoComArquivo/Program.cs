edb0c420a60b22c4bd694aa4c13f3582165b9c9a
Andrey Dario <andreydario26@gmail.com>
2022-01-27 11:11:09 -0300

    Inciando modulo 13 - File and FileInfo

---
+++ Modulo 13/TrabalhandoComArquivo/TrabalhandoComArquivo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TrabalhandoComArquivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourchPath = @"d:\File.txt";
+            string targetPath = @"d:\File1.txt";
+
+            //o @ foi utlizado para evitar \\ 
+
+            try
+            {
+                FileInfo f1 = new FileInfo(sourchPath);
+                f1.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourchPath);
+                
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+               
+            
+            }catch(IOException e){
+                Console.WriteLine("An error occurrd");
+                Console.WriteLine(e.Message);
+            }
+        
+        
+        
+        
+        
+        }
+    }
+}
