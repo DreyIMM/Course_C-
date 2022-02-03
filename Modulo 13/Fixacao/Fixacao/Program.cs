10edcee81835819a65857cdf22c44d40d8b85880
Andrey Dario <andreydario26@gmail.com>
2022-02-03 20:02:48 -0300

    Trabalhando com pasta

---
+++ Modulo 13/Fixacao/Fixacao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Fixacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourchPath = @"d:\myfolder\File.txt";
+
+            try
+            {
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
