a462661fdf3992c3777c2518737d0796efaf6844
Andrey Dario <andreydario26@gmail.com>
2022-02-09 22:28:00 -0300

    Trabalhando com camada de service

---
+++ Modulo 14/Modulo 13/ProjectOfServiceNoInterface/ProjectOfServiceNoInterface/Services/BrazilTaxServices.cs
@@ -0,0 +1,21 @@
+﻿namespace ProjectOfServiceNoInterface.Services
+{
+    internal class BrazilTaxServices
+    {
+       
+        public double Tax(double amount)
+        {
+            if(amount <= 100.00)
+            {
+                return amount * 0.2;
+            }
+            else
+            {
+                return amount * 0.15;
+            }
+        }
+    
+    
+    
+    }
+}
