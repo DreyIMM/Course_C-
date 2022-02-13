34b6f7c01387f4055af784f04d7e174943cd5b4a
Andrey Dario <andreydario26@gmail.com>
2022-02-12 23:42:17 -0300

    Incluindo classe Entities e Services

---
+++ Modulo 14/InterfaceExercicio/InterfaceExercicio/Entities/Contracts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExercicio.Entities
+{
+    internal class Contracts
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set;}
+        public Installment Installment { get; set; }
+
+        public Contracts(int number, DateTime date, double totalValue, Installment installment)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+            Installment = installment;
+        }
+    }
+}
+    
