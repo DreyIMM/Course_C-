429dbaaa88190146a4a4db144086993ff95ec1c5
Andrey Dario <andreydario26@gmail.com>
2022-02-16 13:00:07 -0300

    Fase com erro: falta do IComparable

---
+++ Modulo 14/Utilizando_Interface_IComparable/Utilizando_Interface_IComparable/Entities/Employees.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Utilizando_Interface_IComparable.Entities
+{
+    internal class Employees
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employees(string csvEmployee)
+        {
+            string[] vect = csvEmployee.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Name + ",  " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Modulo 14/Utilizando_Interface_IComparable/Utilizando_Interface_IComparable/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Utilizando_Interface_IComparable.Entities;
+namespace Utilizando_Interface_IComparable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"d:\in.txt";
+
+            try
+            {
+                //Crio um objeto StreamReader e faço a leitura do path
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employees> list = new List<Employees>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employees(sr.ReadLine()));   
+                    }
+                    //Ordenar a lista em ordem alfabetica
+                    list.Sort();
+                    foreach (Employees emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error ocurrred");
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
