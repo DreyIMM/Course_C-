b64f3c69a8344209fefbc109db47332345dfcd4c
Andrey Dario <andreydario26@gmail.com>
2022-02-15 22:38:53 -0300

    Feito com clase abstract

--- Modulo 14/InterfaceComClasseAbstract/InterfaceComClasseAbstract/Program.cs
+++ Modulo 14/InterfaceComClasseAbstract/InterfaceComClasseAbstract/Program.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using InterfaceComClasseAbstract.Models.Entities;
+using InterfaceComClasseAbstract.Models.Enums;
 namespace InterfaceComClasseAbstract
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Shape s1 = new Circle()
+            {
+                Color = Color.Black,
+                Radius = 2.0
+            };
+
+
+            Shape s2 = new Rectangle()
+            {
+                Width =2.5,
+                Height = 4.2,
+                Color = Color.White,
+            };
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
         }
     }
 }
