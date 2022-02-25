91f8fe19dc32cc2e5ebdb544344b70d155704fd2
Andrey Dario <andreydario26@gmail.com>
2022-02-24 21:32:03 -0300

    Resolvendo a restricao

--- Modulo 15/GenerecisRestrision/GenerecisRestrision/Program.cs
+++ Modulo 15/GenerecisRestrision/GenerecisRestrision/Program.cs
@@ -4,27 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GenerecisRestrision.Service;
+using GenerecisRestrision.Entites;
+using System.Globalization;
 namespace GenerecisRestrision
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List <int> list = new List<int>();
+            List <Product> list = new List<Product>();
 
             Console.Write("Repeticao ");
             int n = int.Parse(Console.ReadLine());
 
             for(int i = 0; i < n; i++)
             {
-                Console.Write("Valor: ");
-                int value = int.Parse(Console.ReadLine());  
-                list.Add(value);
+                string[] vs = Console.ReadLine().Split(',');
+                string name = vs[0];
+                double price = double.Parse(vs[1], CultureInfo.InvariantCulture) ;
+
+                list.Add(new Product(name,price));
             }
 
             CalculationService calculationService = new CalculationService();
 
-            int max = calculationService.Max(list);
+            Product max = calculationService.Max(list);
 
             Console.WriteLine("Max ");
             Console.WriteLine(max);
