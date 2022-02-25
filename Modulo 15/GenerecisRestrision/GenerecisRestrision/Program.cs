eae50f10954ec4349e3021b7f407fbba5edd5f6e
Andrey Dario <andreydario26@gmail.com>
2022-02-24 21:13:52 -0300

    Restricao para o uso de generics em alguns caso

--- Modulo 15/GenerecisRestrision/GenerecisRestrision/Program.cs
+++ Modulo 15/GenerecisRestrision/GenerecisRestrision/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using GenerecisRestrision.Service;
 namespace GenerecisRestrision
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            List <int> list = new List<int>();
+
+            Console.Write("Repeticao ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Valor: ");
+                int value = int.Parse(Console.ReadLine());  
+                list.Add(value);
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            int max = calculationService.Max(list);
+
+            Console.WriteLine("Max ");
+            Console.WriteLine(max);
+
+
+
+
         }
     }
 }
