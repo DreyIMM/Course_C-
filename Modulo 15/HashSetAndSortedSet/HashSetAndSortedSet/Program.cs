5260b8ad4a019f10403c681ca8d57c7630c7b92b
Andrey Dario <andreydario26@gmail.com>
2022-03-03 11:57:32 -0300

    Trabalhando com HashSet

---
+++ Modulo 15/HashSetAndSortedSet/HashSetAndSortedSet/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSetAndSortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciamos um conjunto VAZIO
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            //Deve ser feito pelo forEach, já que o conjunto não possui posição, não tem como fazer por FOR
+
+            foreach(string s in set)
+            {
+                Console.WriteLine(s);
+            }
+
+
+
+
+        }
+    }
+}
