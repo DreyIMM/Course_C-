c1e1b81f41872b820405e39dbddfe656ee8138d7
Andrey Dario <andreydario26@gmail.com>
2022-03-02 21:03:09 -0300

    Trabalhando com Equals e GetHasCode

---
+++ Modulo 15/EqualsAndGetHasCode/EqualsAndGetHasCode/Entities/Clientes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualsAndGetHasCode.Entities
+{
+    internal class Clientes
+    {
+        public string Name{ get; set; }
+        public string Email { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Clientes))
+            {
+                return false;
+            }
+            Clientes other = obj as Clientes;
+            return Email.Equals(other.Email);
+
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+
+    }
+}
---
+++ Modulo 15/EqualsAndGetHasCode/EqualsAndGetHasCode/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EqualsAndGetHasCode.Entities;
+namespace EqualsAndGetHasCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Clientes a = new Clientes { 
+            Name = "Andrey",
+            Email ="contato@thenProblem.com"
+   
+            };
+
+            Clientes b = new Clientes
+            {
+                Name = "Andrey",
+                Email = "about@usForever.com"
+
+            };
+
+
+            Console.WriteLine(a.Equals(b));
+            //O equal está ligado ao email
+            //o HasCode é ligado ao Name
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+            //esse == está comparando referência do ponteiro de memoria do objeto
+            Console.WriteLine(a == b);
+
+        }
+    }
+}
