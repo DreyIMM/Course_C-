d3c1ae06e5d892726d828a720b27376080f2cbf4
Andrey Dario <andreydario26@gmail.com>
2022-03-08 22:48:18 -0300

    Exercico pratico com Set

---
+++ Modulo 15/ExercicioPropostoSet/ExercicioPropostoSet/Entities/Students.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPropostoSet.Entities
+{
+    internal class Students
+    {
+        public int Cod_Studentes { get; set; }
+
+      
+
+        public Students(int cod_Studentes)
+        {
+            Cod_Studentes = cod_Studentes;
+          
+        }
+
+        public override int GetHashCode()
+        {
+            return Cod_Studentes.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Students))
+            {
+                return false;
+            }
+
+            Students students = (Students)obj;
+            return Cod_Studentes.Equals(students.Cod_Studentes);
+
+
+
+        }
+
+
+
+    }
+}
---
+++ Modulo 15/ExercicioPropostoSet/ExercicioPropostoSet/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExercicioPropostoSet.Entities;
+
+
+namespace ExercicioPropostoSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            HashSet<Students> stuts = new HashSet<Students>();
+
+            Console.Write("Quantos estudantes no curso A ");
+            int valueA = int.Parse(Console.ReadLine());
+            for(int i = 0; i < valueA; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                stuts.Add(new Students(value));
+            }
+            
+            Console.Write("Quantos estudantes no curso B ");
+            int valueB = int.Parse(Console.ReadLine());
+            for (int i = 0; i < valueB; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                stuts.Add(new Students(value));
+            }
+
+            Console.Write("Quantos estudantes no curso C ");
+            int valueC = int.Parse(Console.ReadLine());
+            for (int i = 0; i < valueC; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                stuts.Add(new Students(value));
+            }
+
+            Console.WriteLine("Total de estudante para esse professor: " +stuts.Count);
+
+        }
+    }
+}
