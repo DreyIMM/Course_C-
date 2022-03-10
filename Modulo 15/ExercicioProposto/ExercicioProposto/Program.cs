532670d2e6a5d29ca2af8cb1cc44e698ddea2ef1
Andrey Dario <andreydario26@gmail.com>
2022-03-09 22:30:25 -0300

    Done: Dictionary

---
+++ Modulo 15/ExercicioProposto/ExercicioProposto/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ExercicioProposto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"d:\in.txt";
+
+            
+
+
+            try
+            {
+                
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> CandidatoVoto = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        int value = int.Parse(line[1]);
+                        string key = line[0];
+
+
+                        if (CandidatoVoto.ContainsKey(key))
+                        {
+                            CandidatoVoto[key] += value;
+                        }
+                        else
+                        {
+                            CandidatoVoto[key] = value;
+                        }                                           
+                       
+
+                    }
+
+
+
+
+                    
+                    foreach(KeyValuePair<string,int> people in CandidatoVoto)
+                    {
+                        Console.WriteLine(people.Key + ": " + people.Value);
+                    }
+
+
+                }
+
+
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
