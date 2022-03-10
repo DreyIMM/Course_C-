5d1fe47c0b2dc7360564df32969cb6c01d0d125c
Andrey Dario <andreydario26@gmail.com>
2022-03-10 20:30:05 -0300

    Trabalhando com extension methods

---
+++ Modulo 16/ExtensionMethodWDateTime/ExtensionMethodWDateTime/Extensions/DateTimeExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+namespace ExtensionMethodWDateTime.Extensions
+{
+    static class DateTimeExtension
+    {
+
+
+        //colocando meus extension methdos
+
+        //o que determina que esse metodo é extensão do tipo dateTime (É o primeiro parametro no metodo que precisa ter o this
+        public static string ElapsedTime(this DateTime thisObjs)
+        {
+            //tempo decorrido em mil.segundos
+
+            TimeSpan duration = DateTime.Now - thisObjs.Date;
+
+           if(duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            }
+            else{
+
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " Days" ;
+
+
+            }
+
+        }
+        
+
+
+    }
+}
---
+++ Modulo 16/ExtensionMethodWDateTime/ExtensionMethodWDateTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionMethodWDateTime.Extensions;
+namespace ExtensionMethodWDateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime dt = new DateTime(2022, 03, 09 , 8, 10, 45);
+            
+            Console.WriteLine(dt.ElapsedTime());
+
+
+        }
+    }
+}
