aafa91355fb3d69f9f3accf98b79fb88cf67e9a9
Andrey Dario <andreydario26@gmail.com>
2022-03-14 17:37:12 -0300

    Trabalhando com delegate

---
+++ Modulo 17/TypeDelagate/TypeDelagate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TypeDelagate.Services;
+namespace TypeDelagate
+{
+    //declarando o Delegate
+    //Referencia para um função com dois params
+    //caso a assinatura da função passada seja diferente, terá erro
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a = 10;
+            double b = 12;
+
+            double result = CalculationService.Sum(a, b);
+            Console.WriteLine(result);
+
+            //declarando um objeto Delegate
+
+            //Meu delegate OP é uma referencia para função soma
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double resultDelegate = op(a, b);
+
+            Console.WriteLine("Whit delegate " +resultDelegate);
+
+
+
+
+
+
+
+
+        }
+    }
+}
