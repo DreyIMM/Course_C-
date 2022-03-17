71e1a1ccc49a6e79ad80c86efac302c76e5cb399
Andrey Dario <andreydario26@gmail.com>
2022-03-17 20:13:30 -0300

    Trabalhando com o Delegate multicast

---
+++ Modulo 17/MulticastDelegate/MulticastDelegate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MulticastDelegate.Services;
+namespace MulticastDelegate
+{
+    //declarando o delegate
+    delegate void BinaryNumericOpertion(double n1,double n2);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOpertion op = CalculationService.ShowSum;
+
+            //Multicast
+            op += CalculationService.ShowMax;
+
+            op.Invoke(a, b);
+
+            //Invocando um Multicast delegate com o Invoke
+
+        }
+    }
+}
