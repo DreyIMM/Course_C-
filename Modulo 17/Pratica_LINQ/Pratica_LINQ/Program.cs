fab72abe8521d608762a7d53f5eb7967b097ef6f
Andrey Dario <andreydario26@gmail.com>
2022-03-21 11:18:52 -0300

    Inciando estudos com LINQ

---
+++ Modulo 17/Pratica_LINQ/Pratica_LINQ/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Specify the data source
+            int[] numbers = new int[] { 1, 2, 3, 4, 5};
+
+            //Define the query expression
+            var result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+           //Execute the query
+
+          foreach(int x in result)
+            {
+                Console.WriteLine(x);
+            }
+
+
+
+
+
+        }
+    }
+}
