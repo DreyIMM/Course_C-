19390a188766439d1c2e204cc4458450cb6ae4df
Andrey Dario <andreydario26@gmail.com>
2022-03-22 20:38:31 -0300

    Desafio com LINQ

---
+++ Modulo 17/ExercicioFixacao_LINQ/ExercicioFixacao_LINQ/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+using ExercicioFixacao_LINQ.Entities;
+namespace ExercicioFixacao_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"d:\in.txt";
+
+            Console.Write("Insira o salário: ");
+            double salary_avg = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            List<Employee> list = new List<Employee>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    string email = fields[1];
+                    double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+
+                    list.Add(new Employee(name, email, salary));
+
+                }
+            }
+
+            var emails = list.Where(p => p.Salary > salary_avg).OrderBy(p => p.Name).Select(p => p.Email);
+
+            Console.WriteLine();
+            foreach(String emp in emails)
+            {
+                Console.WriteLine(emp);
+            }
+
+            var avg = list.Where(p => p.Name[0] == 'M').Select(p => p.Salary).Sum();
+            Console.WriteLine();
+            Console.WriteLine("Média " + avg.ToString("F2", CultureInfo.InvariantCulture));
+            
+
+        }
+    }
+}
---
+++ Modulo 17/ExercicioResolvido_LINQ/ExercicioResolvido_LINQ/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExercicioResolvido_LINQ.Entities;
+using System.IO;
+using System.Globalization;
+namespace ExercicioResolvido_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           // Console.Write("Digite o caminho do arquivo");
+           //string path = Console.ReadLine();
+
+            string path = @"d:\in.txt";
+
+            List<Product> prd = new List<Product>();
+
+            try
+            {
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1],CultureInfo.InvariantCulture);
+
+                        prd.Add(new Product(name, price));
+                        
+                    }
+                }
+
+            }catch (Exception ex)
+            {
+                Console.WriteLine("Message: " +ex);
+            }
+           
+
+            //média dos produtos
+            var avg = prd.Select(p => p.Price).DefaultIfEmpty().Average();
+            Console.WriteLine("Média = " +avg.ToString("F2", CultureInfo.InvariantCulture));
+           
+            //Nomes dos produtos que estão abaixdo da media E em ordem decrescente
+
+            var names = prd.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+
+            Console.WriteLine("Produtos");
+
+            foreach(String name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+
+
+
+
+        }
+    }
+}
